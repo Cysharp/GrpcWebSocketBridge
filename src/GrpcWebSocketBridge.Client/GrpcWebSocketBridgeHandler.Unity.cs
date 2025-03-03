3df971ba1b7ac052bbb156f76cd682ade9d0ac6c
Mayuki Sawatari <mayuki+github@misuzilla.org>
2025-03-03 19:20:37 +0900

    Separate implementation for each platform

--- src/GrpcWebSocketBridge.Client/GrpcWebSocketBridgeHandler.Unity.cs
+++ src/GrpcWebSocketBridge.Client/GrpcWebSocketBridgeHandler.Unity.cs
@@ -1,25 +1,14 @@
+#if UNITY_2022_1_OR_NEWER
 using System;
 using System.Collections.Generic;
 using System.IO.Pipelines;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using GrpcWebSocketBridge.Client.WebSockets;
 
 namespace GrpcWebSocketBridge.Client
 {
-    public partial class GrpcWebSocketBridgeHandler
-    {
-        private static TaskCompletionSource<bool> CreateHeadersTaskCompletionSource() => new TaskCompletionSource<bool>();
-    }
-
-#if NON_UNITY
-    public partial class GrpcWebSocketBridgeHandler
-    {
-        private static PipeOptions PipeOptions { get; } = new PipeOptions();
-
-        private static IClientWebSocket CreateClientWebSocket() => new SystemNetWebSocketsClientWebSocket();
-    }
-#else
     public partial class GrpcWebSocketBridgeHandler
     {
         private static PipeOptions PipeOptions { get; } = new PipeOptions(readerScheduler: PipeScheduler.Inline, writerScheduler: PipeScheduler.Inline, useSynchronizationContext: false);
@@ -30,6 +19,12 @@ namespace GrpcWebSocketBridge.Client
 #else
             new SystemNetWebSocketsClientWebSocket();
 #endif
+
+       public GrpcWebSocketBridgeHandler(bool forceWebSocketMode = false)
+            : base(new GrpcWebSocketBridge.Client.Unity.UnityWebRequestHttpHandler())
+        {
+            _forceWebSocketMode = forceWebSocketMode;
+        }
     }
-#endif
 }
+#endif
